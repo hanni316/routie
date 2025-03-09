a076a101fe1d19d81320b3e25203223c28c1b6ff
jisu <js326js@naver.com>
2025-03-10 00:33:43 +0900

    test 화면에서 리워드 조회, 지급 api 연결

--- routie-mobile/app/src/main/java/com/gbsb/routiemobile/network/RetrofitClient.kt
+++ routie-mobile/app/src/main/java/com/gbsb/routiemobile/network/RetrofitClient.kt
@@ -3,8 +3,10 @@ package com.gbsb.routiemobile.network
 import com.gbsb.routiemobile.api.RewardApiService
 import com.gbsb.routiemobile.dto.CaloriesRequest
 import com.gbsb.routiemobile.dto.RewardResponse
+import okhttp3.Request
 import okio.Timeout
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,45 +26,38 @@ object RetrofitClient {
         retrofit.create(RewardApiService::class.java)
     }
 
-    // 서버 없이 테스트할 때 사용할 Mock API
+    // 🛠 Mock API: 서버 없이 개발할 수 있도록 가짜 데이터 제공
     private val mockInstance: RewardApiService = object : RewardApiService {
-        override fun getUserReward(userID: Int): Call<RewardResponse> {
+
+        override fun getUserReward(userId: String): Call<RewardResponse> {
             return object : Call<RewardResponse> {
                 override fun execute(): Response<RewardResponse> {
-                    val mockResponse = RewardResponse(userId = 1, totalGold = 100, totalSteps = 5000)
+                    val mockResponse = RewardResponse(userId = userId, totalGold = 500, totalSteps = 10000)
                     return Response.success(mockResponse)
                 }
-                override fun enqueue(callback: retrofit2.Callback<RewardResponse>?) {}
+                override fun enqueue(callback: Callback<RewardResponse>?) {}
                 override fun isExecuted(): Boolean = false
                 override fun cancel() {}
                 override fun isCanceled(): Boolean = false
                 override fun clone(): Call<RewardResponse> = this
-                override fun request(): okhttp3.Request = okhttp3.Request.Builder().url(BASE_URL).build()
-                override fun timeout(): Timeout {
-                    TODO("Not yet implemented")
-                }
+                override fun request(): Request = Request.Builder().url(BASE_URL).build()
+                override fun timeout(): Timeout = Timeout.NONE
             }
         }
 
-        override fun getUserReward(userId: Long): Call<RewardResponse> {
-            return getUserReward(userId.toInt()) // 동일한 Mock 데이터 반환
-        }
-
-        override fun sendCalories(userId: Int, request: CaloriesRequest): Call<RewardResponse> {
+        override fun sendCalories(userId: String, request: CaloriesRequest): Call<RewardResponse> {
             return object : Call<RewardResponse> {
                 override fun execute(): Response<RewardResponse> {
-                    val mockResponse = RewardResponse(userId = 1, totalGold = 100, totalSteps = 5000)
+                    val mockResponse = RewardResponse(userId = userId, totalGold = 700, totalSteps = 15000)
                     return Response.success(mockResponse)
                 }
-                override fun enqueue(callback: retrofit2.Callback<RewardResponse>?) {}
+                override fun enqueue(callback: Callback<RewardResponse>?) {}
                 override fun isExecuted(): Boolean = false
                 override fun cancel() {}
                 override fun isCanceled(): Boolean = false
                 override fun clone(): Call<RewardResponse> = this
-                override fun request(): okhttp3.Request = okhttp3.Request.Builder().url(BASE_URL).build()
-                override fun timeout(): Timeout {
-                    TODO("Not yet implemented")
-                }
+                override fun request(): Request = Request.Builder().url(BASE_URL).build()
+                override fun timeout(): Timeout = Timeout.NONE
             }
         }
     }
