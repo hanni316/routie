cd47c52a4cfa55d425c5ed15c973b11b50736fff
yellowsung <shhwang1229@gmail.com>
2025-03-17 01:09:50 +0900

    운동 루틴 프래그먼트 생성 및 xml 코드 추가. 버튼 이벤트 안 넣어놈.버튼은 만들어 두었으니 기능을 프래그먼트에 넣으세요. 메인화면에서 별 아이콘 누르면 루틴 생성 프래그먼트로 이동.

--- routie-mobile/app/src/main/java/com/gbsb/routiemobile/fragment/MainFragment.kt
+++ routie-mobile/app/src/main/java/com/gbsb/routiemobile/fragment/MainFragment.kt
@@ -28,9 +28,10 @@ class MainFragment : Fragment() {
         val buttonProfile = view.findViewById<ImageButton>(R.id.btn_profile)
         val textView: TextView = view.findViewById(R.id.txt_nowdate)
         val btnSelectDate: ImageButton = view.findViewById(R.id.btn_selectdate)
-        val buttonTest: Button = view.findViewById(R.id.buttontest)
+        //val buttonTest: Button = view.findViewById(R.id.buttontest)
         val btnBell: ImageButton = view.findViewById(R.id.btn_bell)
         val bubble2: ImageView = view.findViewById(R.id.img_noticefield)
+        val starButton: ImageButton = view.findViewById(R.id.star_icon)
 
         // ✅ 알림 버튼 클릭 시 토글
         btnBell.setOnClickListener {
@@ -49,8 +50,13 @@ class MainFragment : Fragment() {
         }
 
         // ✅ 테스트 버튼 클릭 시 LoginActivity 이동
-        buttonTest.setOnClickListener {
-            findNavController().navigate(R.id.LoginFragment)
+//        buttonTest.setOnClickListener {
+//            findNavController().navigate(R.id.LoginFragment)
+//        }
+
+        // 별 버튼 누르면 MakingroutineFragment 이동
+        starButton.setOnClickListener {
+            findNavController().navigate(R.id.MakingroutineFragment)
         }
 
         // ✅ 현재 날짜 가져오기
