4cbf8a03cf2abaf03c233211661633d8050f7a1b
jisu <js326js@naver.com>
2025-03-23 16:34:44 +0900

    회원정보 변경, 탈퇴 완료

--- routie-server/src/main/java/com/gbsb/routie_server/repository/RoutineRepository.java
+++ routie-server/src/main/java/com/gbsb/routie_server/repository/RoutineRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
     List<Routine> findByUser_UserId(String userId); // 특정 사용자의 루틴 목록 조회
+    void deleteByUser_UserId(String userId);
 }
