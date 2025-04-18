82feb61388302d709f251f39a4a0af2418e92919
yellowsung <shhwang1229@gmail.com>
2025-04-18 23:08:19 +0900

    (완료) 운동리스트 불러오는 부분 수정

--- routie-mobile/routie-wear/src/main/java/com/example/routie_wear/ui/WorkoutListScreen.kt
+++ routie-mobile/routie-wear/src/main/java/com/example/routie_wear/ui/WorkoutListScreen.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -22,39 +26,61 @@ fun WorkoutListScreen(
 ) {
     val workouts = viewModel.workoutList
     val selectedWorkout = viewModel.selectedWorkout
+    val selectedRoutineId = viewModel.selectedRoutineId
     val context = LocalContext.current
 
-    ScalingLazyColumn {
-        items(workouts) { workout ->
-            val isSelected = workout == selectedWorkout
+    LaunchedEffect(selectedRoutineId) {
+        if (selectedRoutineId != null) {
+            viewModel.loadWorkouts(selectedRoutineId!!)
+        }
+    }
 
-            val alpha by animateFloatAsState(
-                targetValue = if (isSelected) 0.6f else 1f,
-                animationSpec = tween(durationMillis = 250)
+    if (workouts.isEmpty()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                "운동이 비어 있어요!",
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onPrimary
             )
+        }
+    } else {
+        ScalingLazyColumn {
+            items(workouts) { workout ->
+                val isSelected = workout == selectedWorkout
 
-            Chip(
-                modifier = Modifier
-                    .padding(vertical = 4.dp)
-                    .animateContentSize(),
-                label = {
-                    Text(
-                        text = "${workout.name}",
-                        color = MaterialTheme.colors.onSurface.copy(alpha = alpha)
-                    )
-                },
-                onClick = {
-                    VibrationUtil.vibrate(context)  // 진동 효과
-                    viewModel.selectedWorkout = workout
-                    onWorkoutSelected()
-                },
-                colors = ChipDefaults.chipColors(
-                    backgroundColor = if (isSelected)
-                        MaterialTheme.colors.secondary
-                    else
-                        MaterialTheme.colors.surface
+                val alpha by animateFloatAsState(
+                    targetValue = if (isSelected) 0.6f else 1f,
+                    animationSpec = tween(durationMillis = 250)
                 )
-            )
+
+                Chip(
+                    modifier = Modifier
+                        .padding(vertical = 4.dp)
+                        .animateContentSize(),
+                    label = {
+                        Text(
+                            text = "${workout.name}",
+                            color = MaterialTheme.colors.onSurface.copy(alpha = alpha)
+                        )
+                    },
+                    onClick = {
+                        VibrationUtil.vibrate(context)
+                        viewModel.selectedWorkout = workout
+                        onWorkoutSelected()
+                    },
+                    colors = ChipDefaults.chipColors(
+                        backgroundColor = if (isSelected)
+                            MaterialTheme.colors.secondary
+                        else
+                            MaterialTheme.colors.surface
+                    )
+                )
+            }
         }
     }
 }
