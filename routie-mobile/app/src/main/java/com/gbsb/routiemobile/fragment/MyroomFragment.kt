5e76ac5ce3f2e145a6873ce93c7aa8c12d2f68ca
yellowsung <shhwang1229@gmail.com>
2025-04-20 00:40:11 +0900

    스토어 프래그먼트 기본 코드 추가

--- routie-mobile/app/src/main/java/com/gbsb/routiemobile/fragment/MyroomFragment.kt
+++ routie-mobile/app/src/main/java/com/gbsb/routiemobile/fragment/MyroomFragment.kt
@@ -1,7 +1,9 @@
 package com.gbsb.routiemobile.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -21,5 +23,11 @@ class MyroomFragment : Fragment() {
         view.findViewById<ImageButton>(R.id.btn_out).setOnClickListener {
             findNavController().navigate(R.id.action_myroomFragment_to_MainFragment)
         }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_myroom, container, false)
     }
 }
