06f1d8ea18b282224dbbef9836abb6ce6e611cd4
yellowsung <shhwang1229@gmail.com>
2025-05-25 22:53:10 +0900

    갸차부분 화면 수정 및 데이터 전송 기능 추가 +점진적으로 추가중임

--- routie-server/src/main/java/com/gbsb/routie_server/repository/GachaLogRepository.java
+++ routie-server/src/main/java/com/gbsb/routie_server/repository/GachaLogRepository.java
@@ -2,7 +2,11 @@ package com.gbsb.routie_server.repository;
 
 import com.gbsb.routie_server.entity.GachaLog;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GachaLogRepository extends JpaRepository<GachaLog, Long> {
+    //실패횟수 조회용
+    @Query("SELECT COUNT(g) FROM GachaLog g WHERE g.user.userId = :userId AND g.item IS NULL")
+    int countFailuresByUserId(@Param("userId") String userId);
 }
