f90eded82d634a79160156616542b69675492541
yellowsung <shhwang1229@gmail.com>
2025-11-03 21:06:31 +0900

    -걸음수 api(걸음루틴 연관)

--- routie-server/src/main/java/com/gbsb/routie_server/dto/GachaResultDto.java
+++ routie-server/src/main/java/com/gbsb/routie_server/dto/GachaResultDto.java
@@ -1,6 +1,5 @@
 package com.gbsb.routie_server.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
