cfe010d525d533f740c30ac6da70d63b71c95414
yellowsung <shhwang1229@gmail.com>
2025-11-03 21:05:12 +0900

    -걸음수 api(걸음루틴 연관)

--- routie-server/src/main/java/com/gbsb/routie_server/controller/WalkSessionController.java
+++ routie-server/src/main/java/com/gbsb/routie_server/controller/WalkSessionController.java
@@ -1,4 +1,35 @@
 package com.gbsb.routie_server.controller;
 
+import com.gbsb.routie_server.dto.*;
+import com.gbsb.routie_server.service.WalkSessionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/walk-sessions")
 public class WalkSessionController {
+
+    private final WalkSessionService walkSessionService;
+
+    public WalkSessionController(WalkSessionService walkSessionService) {
+        this.walkSessionService = walkSessionService;
+    }
+
+    // 세션 시작
+    @PostMapping("/start")
+    public ResponseEntity<WalkSessionStartResponseDto> startSession(
+            @RequestBody WalkSessionStartRequestDto dto
+    ) {
+        WalkSessionStartResponseDto response = walkSessionService.startSession(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    // 세션 종료
+    @PostMapping("/end")
+    public ResponseEntity<WalkSessionEndResponseDto> endSession(
+            @RequestBody WalkSessionEndRequestDto dto
+    ) {
+        WalkSessionEndResponseDto response = walkSessionService.endSession(dto);
+        return ResponseEntity.ok(response);
+    }
 }
